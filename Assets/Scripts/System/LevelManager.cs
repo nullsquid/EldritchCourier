c90f06270517910fb41085ba8f8bf797d5ae1bea
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2016-04-18 08:16:11 -0400

    added objects to map scene

---
+++ Assets/Scripts/System/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(int levelIndex)
+    {
+        Application.LoadLevel(levelIndex);
+    }
+}
